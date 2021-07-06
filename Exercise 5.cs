2e1bc0ba7501c8e45772ae6380b50845a96d04b9
Adamserty <41434390+Adamserty@users.noreply.github.com>
2021-07-06 09:40:24 +0000

    Add files via upload

---
+++ Exercise 5.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Paints
+{
+    string color = "red";
+    static void Main(string[] args)
+    {
+        Paints myObj1 = new Paints();
+        Paints myObj2 = new Paints();
+        Console.WriteLine(myObj1.color);
+        Console.WriteLine(myObj2.color);
+    }
+}

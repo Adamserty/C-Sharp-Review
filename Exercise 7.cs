d76d8368a8deafe1bf0ad4fadbd3a56b6af6f32a
Adamserty <41434390+Adamserty@users.noreply.github.com>
2021-07-06 10:40:39 +0000

    Add files via upload

---
+++ Exercise 7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class CarSpeed
+{
+    string color;                 // field
+    int maxSpeed;                 // field
+    public void fullThrottle()    // method
+    {
+        Console.WriteLine("The car is going as fast as it can!");
+    }
+
+    static void Main(string[] args)
+    {
+        CarSpeed myObj = new CarSpeed();
+        myObj.fullThrottle();  // Call the method
+    }
+}

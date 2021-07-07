e3b6786042c5c76b52bedcbf02a791a85fa2f43b
Adamserty <41434390+Adamserty@users.noreply.github.com>
2021-07-07 12:19:21 +0000

    Add files via upload

---
+++ Exercise 8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Car
+{
+    string model;
+    string color;
+    int year;
+
+    static void Main(string[] args)
+    {
+        Car Chevrolet = new Car();
+        Chevrolet.model = "Camaro";
+        Chevrolet.color = "red";
+        Chevrolet.year = 1969;
+
+        Car Buick = new Car();
+        Buick.model = "Encore";
+        Buick.color = "white";
+        Buick.year = 2005;
+
+        Console.WriteLine(Chevrolet.model);
+        Console.WriteLine(Buick.model);
+    }
+}
